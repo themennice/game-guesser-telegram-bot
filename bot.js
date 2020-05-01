821cfca2cc57179c88baf2023cf4b798ef805508
themennice <dziubiiden@gmail.com>
2020-05-01 16:18:56 -0700

    shuffled the list for showing all options

--- bot.js
+++ bot.js
@@ -47,6 +47,7 @@ bot.onText(/^\/wakeup (.+)$/, function (msg, match) {
   //var all_chars = shuffle(basketball.concat(football, gymnastics, surfing, boxing, tennis));
   var all_chars = basketball.concat(football, gymnastics);
   all_chars = shuffle(all_chars);
+  
   // select a random sport from the list of sports
   var rand_selected_sport = sports[Math.floor(Math.random() * sports.length)];
   // delete the chosen sport from the list so that next time no repeats occur
@@ -113,3 +114,20 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message: ' + msg.text.toString());
 });*/
+
+
+/**
+ * Fisher-Yates Shuffle
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
