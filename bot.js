fb9e6b6183e791370ab42efebb2798afcea3d2fc
themennice <dziubiiden@gmail.com>
2020-05-01 11:07:32 -0700

    testing echo and message

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
 // start command
 bot.onText(/^\/wakeup (.+)$/, function (msg, match) {
   var name = match[1];
-  bot.sendMessage(msg.chat.id, '\tWelcome to Game Guesser Bot ' + name + '! \nI will think of an Olympic game and you will try to guess it. \n \n Simply type a characteristic of the sport and I will say yes if it belongs to my guessed sport, and no otherwise.\n \n After 10 questions simply type the name of the game you think I guessed! \n \nLet us get started! I have selected a game. Make a guess.').then(function () {
+  bot.sendMessage(msg.chat.id, '\tWelcome to Game Guesser Bot, ' + name + '! \nI will think of an Olympic game and you will try to guess it. \n \n Simply type a characteristic of the sport and I will say yes if it belongs to my guessed sport, and no otherwise.\n \n After 10 questions simply type the name of the game you think I guessed! \n \nLet us get started! I have selected a game. Make a guess.').then(function () {
     // reply sent!
   });
 });
@@ -41,4 +41,26 @@ bot.onText(/^\/multiply((\s+\d+)+)$/, function (msg, match) {
   bot.sendMessage(msg.chat.id, result).then(function () {
     // reply sent!
   });
+});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
 });
