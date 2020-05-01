9f82794fb1521f5b9e17abea23f285c825dbd5ff
themennice <dziubiiden@gmail.com>
2020-05-01 12:01:00 -0700

    testing random sports

--- bot.js
+++ bot.js
@@ -24,13 +24,32 @@ bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
   });
 });
 
-// start command
+
+// start command for the guess game
 bot.onText(/^\/wakeup (.+)$/, function (msg, match) {
   var name = match[1];
   bot.sendMessage(msg.chat.id, '\tWelcome to Game Guesser Bot, ' + name + '! \nI will think of an Olympic game and you will try to guess it. \n \n Simply type a characteristic of the sport and I will say yes if it belongs to my guessed sport, and no otherwise.\n \n After 10 questions simply type the name of the game you think I guessed! \n \nLet us get started! I have selected a game. Make a guess.').then(function () {
   // reply sent!
 
+  // declare a list of the Olympic sports for a given demonstration
+  var sports = ['basketball', 'football', 'gymnastics', 'surfing', 'boxing', 'tennis'];
 
+  // intiate and populate Olympic sports lists - basketball
+  var basketball = ['court', 'basket', 'team', 'basketball', 'timed'];
+  var football = ['goal', 'goalkeeper', 'team', 'doubles', 'grass', 'football', 'field', 'stadium', 'halves'];
+  var vargymnastics = ['gym', 'flexibility', 'score', 'jumping'];
+  var surfing = ['surfboard', 'outside', 'water', 'waves', 'singles', 'score'];
+  var boxing = ['knockout', 'timed', 'punches', 'money', 'concussions', 'danger'];
+  var tennis = ['court', 'singles', 'doubles', 'outdoor', 'ball', 'racket', 'net', 'umbrellas', 'helpers', 'rating'];
+
+  // select a random sport from the list of sports
+  var random_sport = sports[Math.floor(Math.random() * sports.length)];
+  // delete the chosen sport from the list so that next time no repeats occur
+  const index = array.indexOf(random_sport);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+  console.log(sports);
   });
 });
 
@@ -60,9 +79,9 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on('message', (msg) => {
+/*bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message: ' + msg.text.toString());
-});
+});*/
