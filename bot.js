ff62fa1b3d8c9b60916d23861b3d337a31cbd1bd
themennice <dziubiiden@gmail.com>
2020-05-01 16:15:17 -0700

    shuffled the list for showing all options

--- bot.js
+++ bot.js
@@ -46,6 +46,7 @@ bot.onText(/^\/wakeup (.+)$/, function (msg, match) {
 
   //var all_chars = shuffle(basketball.concat(football, gymnastics, surfing, boxing, tennis));
   var all_chars = basketball.concat(football, gymnastics);
+  all_chars = shuffle(all_chars);
   // select a random sport from the list of sports
   var rand_selected_sport = sports[Math.floor(Math.random() * sports.length)];
   // delete the chosen sport from the list so that next time no repeats occur
