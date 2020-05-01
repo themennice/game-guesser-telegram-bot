b868595b3444e18ebb51eef10677652c36d38fea
themennice <dziubiiden@gmail.com>
2020-05-01 11:45:39 -0700

    modified on message

--- bot.js
+++ bot.js
@@ -28,7 +28,9 @@ bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
 bot.onText(/^\/wakeup (.+)$/, function (msg, match) {
   var name = match[1];
   bot.sendMessage(msg.chat.id, '\tWelcome to Game Guesser Bot, ' + name + '! \nI will think of an Olympic game and you will try to guess it. \n \n Simply type a characteristic of the sport and I will say yes if it belongs to my guessed sport, and no otherwise.\n \n After 10 questions simply type the name of the game you think I guessed! \n \nLet us get started! I have selected a game. Make a guess.').then(function () {
-    // reply sent!
+  // reply sent!
+
+
   });
 });
 
@@ -59,8 +61,8 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
-  const chatId = msg.chat.message;
+  const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message: ' + chatId);
+  bot.sendMessage(chatId, 'Received your message: ' + msg.text.toString());
 });
