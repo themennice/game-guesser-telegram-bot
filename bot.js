1b64535fc954c503f2b4f1906bb56c313b657e4b
themennice <dziubiiden@gmail.com>
2020-05-01 11:15:12 -0700

    modified on message

--- bot.js
+++ bot.js
@@ -62,5 +62,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'Received your message: ' + chatId);
 });
