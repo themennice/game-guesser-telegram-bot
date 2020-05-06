c59fa1a719b57f3bea350f762d24282d0ea53cc9
themennice <dziubiiden@gmail.com>
2020-05-06 15:29:47 -0700

    testing last function

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
 bot.onText(/^\/wakeup (.+)$/, function (msg, match) {
 
   var name = match[1];
-  bot.sendMessage(msg.chat.id, '\tWelcome to Game Guesser Bot, ' + name.charAt(0).toUpperCase() + name.substring(1).toLowerCase(); + '! \nI will think of an Olympic game and you will try to guess it. \n \n Simply type a characteristic of the sport and I will say yes if it belongs to my guessed sport, and no otherwise.\n \n After 10 questions simply type the name of the game you think I guessed! \n \nLet us get started! I have selected a game. Make a guess.').then(function () {
+  bot.sendMessage(msg.chat.id, '\tWelcome to Game Guesser Bot, ' + name.charAt(0).toUpperCase() + name.substring(1).toLowerCase() + '! \nI will think of an Olympic game and you will try to guess it. \n \n Simply type a characteristic of the sport and I will say yes if it belongs to my guessed sport, and no otherwise.\n \n After 10 questions simply type the name of the game you think I guessed! \n \nLet us get started! I have selected a game. Make a guess.').then(function () {
   bot.sendMessage(msg.chat.id, 'You could also type \[show all options\] (without brackets) to see all available sport characteristics.');
   // reply sent!
 
