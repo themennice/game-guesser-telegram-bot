0e3d1e8a1fbe693c3fdfdcc2052a19299472007f
themennice <dziubiiden@gmail.com>
2020-05-07 00:17:52 -0700

    added a recurring message after the limit of rounds is exceeded

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ bot.onText(/^\/ask (.+)$/, function (msg, match) {
       });
     };
   }
-  else finalMessage(msg.chat.id);
+  else bot.sendMessage(msg.chat.id, "\n\nThis is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to restart the game.\n\nYou could also look at this project on github instead:\nhttps://github.com/themennice/game-guesser-telegram-bot");
 });
 
 function play(chatId){
@@ -128,7 +128,7 @@ function play(chatId){
       }
   });
 }
-else finalMessage(msg.chat.id);
+else bot.sendMessage(msg.chat.id, "\n\nThis is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to restart the game.\n\nYou could also look at this project on github instead:\nhttps://github.com/themennice/game-guesser-telegram-bot");
 }
 
 
@@ -137,7 +137,7 @@ bot.on('message', (msg) => {
   if (msg.text.toString().toLowerCase().includes("show all options") && round <= 5) {
     bot.sendMessage(msg.chat.id, shuffle(all_chars).toString());
   }
-  else finalMessage(msg.chat.id);
+  else bot.sendMessage(msg.chat.id, "\n\nThis is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to restart the game.\n\nYou could also look at this project on github instead:\nhttps://github.com/themennice/game-guesser-telegram-bot");
 });
 
 
