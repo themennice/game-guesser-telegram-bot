2b67a0c18fca70ebf81dbac2b1f77d23a9f1cd3e
themennice <dziubiiden@gmail.com>
2020-05-06 21:16:56 -0700

    fixing polling error

--- bot.js
+++ bot.js
@@ -114,47 +114,51 @@ function play(chatId){
     round++;
 
     // check
-    bot.on('message', (msg) => {
+  //   bot.on('message', (msg) => {
 
-      if (msg.text.toString().toLowerCase().includes(rand_selected_sport)) {
-        bot.sendMessage(msg.chat.id, "You are correct! I thought of " + rand_selected_sport + "!").then(function () {
-        if(round == 5) bot.sendMessage(msg.chat.id, "This is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to start all over. You could also look at this project on github instead: https://github.com/themennice/game-guesser-telegram-bot");
-        else
-        bot.sendMessage(msg.chat.id, "If you would like to play again, simply type /play"); });
-        question_num = 0;
-      }
-      else if (sports.includes(msg.text.toString().toLowerCase()) && rand_selected_sport != msg.text.toString().toLowerCase()) {
-        bot.sendMessage(msg.chat.id, "Sorry, " + msg.text.toString().toLowerCase() + " is not the sport I guessed. I selected " + rand_selected_sport + ".").then(function () {
-        if(round > 5) bot.sendMessage(msg.chat.id, "This is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to start all over. You could also look at this project on github instead: https://github.com/themennice/game-guesser-telegram-bot");
-        else
-        bot.sendMessage(msg.chat.id, "If you would like to play again, simply type /play"); });
-        question_num = 0;
-      }
-  });
+  //     if (msg.text.toString().toLowerCase().includes(rand_selected_sport)) {
+  //       bot.sendMessage(msg.chat.id, "You are correct! I thought of " + rand_selected_sport + "!").then(function () {
+  //       if(round == 5) bot.sendMessage(msg.chat.id, "This is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to start all over. You could also look at this project on github instead: https://github.com/themennice/game-guesser-telegram-bot");
+  //       else
+  //       bot.sendMessage(msg.chat.id, "If you would like to play again, simply type /play"); });
+  //       question_num = 0;
+  //     }
+  //     else if (sports.includes(msg.text.toString().toLowerCase()) && rand_selected_sport != msg.text.toString().toLowerCase()) {
+  //       bot.sendMessage(msg.chat.id, "Sorry, " + msg.text.toString().toLowerCase() + " is not the sport I guessed. I selected " + rand_selected_sport + ".").then(function () {
+  //       if(round > 5) bot.sendMessage(msg.chat.id, "This is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to start all over. You could also look at this project on github instead: https://github.com/themennice/game-guesser-telegram-bot");
+  //       else
+  //       bot.sendMessage(msg.chat.id, "If you would like to play again, simply type /play"); });
+  //       question_num = 0;
+  //     }
+  // });
 }
 
-// // implement "show all options" function within the play function so that it can only be called after the game has started
-// bot.on('message', (msg) => {
-//   if (msg.text.toString().toLowerCase().includes("show all options")) {
-//     bot.sendMessage(msg.chat.id, shuffle(all_chars).toString());
-//   }
-// });
+// implement "show all options" function within the play function so that it can only be called after the game has started
+bot.on('message', (msg) => {
+  if (msg.text.toString().toLowerCase().includes("show all options")) {
+    bot.sendMessage(msg.chat.id, shuffle(all_chars).toString());
+  }
+});
 
-// bot.on('message', (msg) => {
-//   if (msg.text.toString().toLowerCase().includes(rand_selected_sport)) {
-//     console.log(rand_selected_sport);
-//     bot.sendMessage(msg.chat.id, "You are correct! I thought of " + rand_selected_sport + "!").then(function () { finalMessage(msg.chat.id) });
-//   }
-//   else if (sports.includes(msg.text.toString().toLowerCase()) && rand_selected_sport != msg.text.toString().toLowerCase()) {
-//     bot.sendMessage(msg.chat.id, "Sorry, " + msg.text.toString().toLowerCase() + " is not the sport I guessed. I selected " + rand_selected_sport + ".").then(function () { finalMessage(msg.chat.id) });
-//   }
-// });
+bot.on('message', (msg) => {
+  if (msg.text.toString().toLowerCase().includes(rand_selected_sport)) {
+    console.log(rand_selected_sport);
+    //bot.sendMessage(msg.chat.id, "You are correct! I thought of " + rand_selected_sport + "!").then(function () { finalMessage(msg.chat.id) });
+    bot.sendMessage(msg.chat.id, "You are correct! I thought of " + rand_selected_sport + "!");
+    finalMessage(msg.chat.id)
+  }
+  else if (sports.includes(msg.text.toString().toLowerCase()) && rand_selected_sport != msg.text.toString().toLowerCase()) {
+    // bot.sendMessage(msg.chat.id, "Sorry, " + msg.text.toString().toLowerCase() + " is not the sport I guessed. I selected " + rand_selected_sport + ".").then(function () { finalMessage(msg.chat.id) });
+    bot.sendMessage(msg.chat.id, "Sorry, " + msg.text.toString().toLowerCase() + " is not the sport I guessed. I selected " + rand_selected_sport + ".");
+    finalMessage(msg.chat.id);
+  }
+});
 
-// function finalMessage(chatId) {
-//   if(round > 5){ bot.sendMessage(chatId, "This is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to start all over. You could also look at this project on github instead: https://github.com/themennice/game-guesser-telegram-bot");}
-//   else { bot.sendMessage(chatId, "If you would like to play again, simply type /play"); }
-//   question_num = 0;
-// }
+function finalMessage(chatId) {
+  if(round > 5){ bot.sendMessage(chatId, "This is the end of round 5. The game is over. Please type in \'/wakeup Your_Name\' if you would like to start all over. You could also look at this project on github instead: https://github.com/themennice/game-guesser-telegram-bot");}
+  else { bot.sendMessage(chatId, "If you would like to play again, simply type /play"); }
+  question_num = 0;
+}
 
 
 /**
